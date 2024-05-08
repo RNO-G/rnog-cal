f4bb0b094fb7c6f3394ff07fcbc9491f5826fa96
Cosmin Deaconu <cozzyd@gmail.com>
2024-05-08 00:45:56 -0500

    add sleep

--- calBoardOn.sh
+++ calBoardOn.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
-if [ ! -d /sys/class/gpio/gpio49 ]; then echo 49 > /sys/class/gpio/export; fi
+if [ ! -d /sys/class/gpio/gpio49 ]; then 
+  echo 49 > /sys/class/gpio/export; 
+  sleep 0.1
+fi
 
 echo high > /sys/class/gpio/gpio49/direction
